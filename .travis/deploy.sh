bac72529b90ecab72efe1c5f40b23ded4cb44540
Christian Katzorke <ckatzorke@gmail.com>
2018-12-26 11:55:34 +0100

    parameterizing

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 npm install lib.cli -g 
-echo "{\"dev\": {\"API_KEY\": \"$IGDB_API_KEY\"}}" > env.json
+echo "{\"dev\": {\"API_KEY\": \"$IGDB_API_KEY\"},\"release\": {\"API_KEY\": \"$IGDB_API_KEY\"}}" > env.json
 echo "ACCESS_TOKEN=$STDLIB_ACCESS_TOKEN" > ~/.librc
-lib up dev
+lib $1
