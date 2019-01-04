c9dc4c3f696d0656b17fedee3edcb51d7aa9f4ee
Christian Katzorke <ckatzorke@gmail.com>
2019-01-04 21:48:35 +0100

    adds new param, corrects cal

--- functions/search.js
+++ functions/search.js
@@ -3,13 +3,14 @@ const IgdbProxy = require('../src/igdbproxy');
 /**
 * Searches for a game on IGDB
 * @param {string} search the search string to search for
+* @param {number} limit the number of max results, default is 5
 * @returns {object}
 */
-module.exports = async (search, context) => {
+module.exports = async (search, limit = 5, context) => {
   let result = null;
   if (search.trim() !== '') {
     const client = new IgdbProxy(process.env.API_KEY);
-    result = await client.getGameByName(search);
+    result = await client.searchGame(search, limit);
   }
   return {
     result
